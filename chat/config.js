ff5dbebfe817cdeb9a1b5c0df29ddafc93f6fbad
VadyChel <zyzel19@gmail.com>
2022-05-04 17:26:18 +0300

    Add config

---
+++ chat/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAxiZXriKMSuvpPUaKtgoydDN0ubcFC2So",
+  authDomain: "vvfirebasechat.firebaseapp.com",
+  projectId: "vvfirebasechat",
+  storageBucket: "vvfirebasechat.appspot.com",
+  messagingSenderId: "604146670792",
+  appId: "1:604146670792:web:9079cb003de564a2e059fd",
+}
--- chat/src/main.js
+++ chat/src/main.js
@@ -2,20 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Config from "../config";
 import { initializeApp } from "firebase/app";
 import {GoogleAuthProvider, onAuthStateChanged, signInWithCredential, getAuth, getIdTokenResult} from "firebase/auth";
 import "@mdi/font/css/materialdesignicons.css";
 import "./assets/main.css"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAxiZXriKMSuvpPUaKtgoydDN0ubcFC2So",
-  authDomain: "vvfirebasechat.firebaseapp.com",
-  projectId: "vvfirebasechat",
-  storageBucket: "vvfirebasechat.appspot.com",
-  messagingSenderId: "604146670792",
-  appId: "1:604146670792:web:9079cb003de564a2e059fd",
-};
-
+const firebaseConfig = Config;
 initializeApp(firebaseConfig);
 
 const auth = getAuth()
